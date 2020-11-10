6695a75a125ad52f24867257381c50ef26c515a0
ryan.bollettino@gmail.com <bollo@BOLLO-LAPPY.localdomain>
2020-11-09 23:35:38 -0600

    Done.

--- variable.rb
+++ variable.rb
@@ -1,2 +1,21 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
+
+greeting = "Hello World"
+
+
+# Otherwise those changes wont register and your tests will fail. So while you had the right code there, the file wasnt saved, which is why the tests were still failing. 
+# not sure if you have been trying to talk but i have volumn down on my com-i kw  for some reason my keyboard wasn't working it was in type-to-text or something
+
+
+# i think in the pre-req labs, it saved automatically. so no idea. 
+# i need some sort of tutorial on this ....but I learned enough to hopefully get me thru the next few labs. 
+#Save and test, save and test. 
+# exactly. the dot in the file should be your cue that the file isnt saved
+# and I don't have to `learn submit` on this, right? 
+# correct, cause I ran learn test for you, thats the command to run...not learn submit
+# right, but once it passes, it will register in Github automatically? or i need to save it a final time?
+# running 'learn test' should run the tests AND push your code up to github
+# if you check your github repo it should show the new code above
+# if i knew what github repo was, I would totally go check that out. 
+# goa head and ipen up your github account
